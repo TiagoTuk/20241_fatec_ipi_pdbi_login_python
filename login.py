78fcc117d5c6be98cfc059a17d1dfd26f751b39c
Tiago Gomes de Barros <tiago.barros596@gmail.com>
2024-03-12 09:55:29 -0300

    feat(ap07): exercicio
    - funcao inserir implantada

--- login.py
+++ login.py
@@ -28,8 +28,52 @@ def existe(usuario):
             result = cursor.fetchone()
             #return True if result != None else False #se result for igual a None, devolvo False, senão devolvo True
             return result != None
-        
-def teste():
-    print(existe(Usuario('admin', 'admin')))
 
-teste()
+def inserir(usuario):
+    with psycopg.connect(
+        host="localhost",
+        port=5432,
+        dbname="20241_fatec_ipi_login_python",
+        user="postgres",
+        password="123456"
+    ) as conexao:
+        with conexao.cursor() as cursor:
+            cursor.execute(
+            'INSERT INTO tb_usuario (login, senha) VALUES (%s, %s)',
+            (usuario.login, usuario.senha))
+            conexao.commit()              
+            # result = cursor.fetchone()
+            # return True if result != None else False #se result for igual a None, devolvo False, senão devolvo True
+            #return result != None
+            print('Usuário criado com sucesso!')
+
+def menu():
+    texto = '0-Sair\n1-Login\n2-Logoff\n3-Novo Usuário\n'
+    #usuário ainda não existe
+    usuario = None
+    op = int(input(texto))
+    while op != 0:
+        if op == 1:
+            login = input('Digite o login: ')
+            senha = input('Digite a senha: ')
+            usuario = Usuario(login, senha)
+            #expressão condicional (if/else de uma linha só)
+            print('Usuário OK!' if existe(usuario) else 'Usuário NOK!')
+        elif op == 2:
+            usuario = None
+            print('Logoff realizado com sucesso')
+        elif op == 3:
+            login = input('Crie o seu login: ')
+            senha = input('Crie sua senha: ')
+            usuario = Usuario(login, senha)
+            inserir(usuario)
+        else:
+            print('Jovem, somente valores entre 0, 1, 2 e 3 plis')
+        op = int(input(texto))
+
+menu()
+
+# def teste():
+#     print(existe(Usuario('admin', 'admin')))
+
+# teste()
